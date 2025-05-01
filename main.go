6541ecd2fa8eee437698cfbc87e54357c9bb9a89
paulnadolny <Paul.Stephen.Nadolny@ibm.com>
2025-05-01 14:49:38 -0500

    cleanup

--- main.go
+++ main.go
@@ -8,17 +8,17 @@ import (
 )
 
 func main() {
-	pipe1reader, pipe1Writer := io.Pipe()
-	pipe2reader, pipe2Writer := io.Pipe()
-	multiWriter := io.MultiWriter(pipe1Writer, pipe2Writer)
+	pipeReader1, pipeWriter1 := io.Pipe()
+	pipeReader2, pipeWriter2 := io.Pipe()
+	multiWriter := io.MultiWriter(pipeWriter1, pipeWriter2)
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		defer pipe2Writer.Close()
-		defer pipe1Writer.Close()
+		defer pipeWriter2.Close()
+		defer pipeWriter1.Close()
 		for x := 1; x < 5; x++ {
-			io.Copy(multiWriter, strings.NewReader("Hello world test stream"))
+			io.Copy(multiWriter, strings.NewReader(fmt.Sprintf("Hello%d", x)))
 		}
 	}()
 
@@ -27,7 +27,7 @@ func main() {
 	go func() {
 		defer wg.Done()
 		defer pipe1TransformedWriter.Close()
-		data, _ := io.ReadAll(pipe1reader)
+		data, _ := io.ReadAll(pipeReader1)
 		pipe1TransformedWriter.Write([]byte(strings.ToUpper(string(data))))
 	}()
 	wg.Add(1)
@@ -42,7 +42,7 @@ func main() {
 	go func() {
 		defer wg.Done()
 		defer pipe2TransformedWriter.Close()
-		data, _ := io.ReadAll(pipe2reader)
+		data, _ := io.ReadAll(pipeReader2)
 		pipe2TransformedWriter.Write([]byte("PREFIX: " + string(data)))
 	}()
 	wg.Add(1)
